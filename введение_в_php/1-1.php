8dc1ca4fdfb60de94571bcdd8bc9300811481bc5
Wafrr <polinka10152007@gmail.com>
2026-04-19 21:39:14 +0300

    Добавление новых кодов для практической работы

---
+++ введение_в_php/1-1.php
@@ -0,0 +1,12 @@
+<?php
+echo "Вывод переменных:\n";
+
+$num = 1234;
+$string = "Да,это текст";
+$bool = TRUE;
+$string2 = "1234";
+$float = 25.89;  
+
+echo $num . "<br/>" . $string . "<br/>" . $bool . "<br/>" . $string2 . "<br/>" . $float;
+
+?>
