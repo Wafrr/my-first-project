217b78d1de971501c6a1cabc5bc2310a08327203
Wafrr <polinka10152007@gmail.com>
2026-04-20 20:47:08 +0300

    Обновление

--- CSRF атаки/profile.php
+++ CSRF атаки/profile.php
@@ -2,7 +2,7 @@
 require 'Csrf.php';
 ?>
 
-<h2>👤 Обновить профиль (POST /profile/update)</h2>
+<h2>Обновить профиль (POST /profile/update)</h2>
 <form method="POST" action="update.php">
     <?php echo Csrf::field(); ?>
     <input type="email" name="email" placeholder="Email" required>
--- CSRF атаки/update.php
+++ CSRF атаки/update.php
@@ -5,7 +5,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
     if (!Csrf::check($_POST['csrf_token'] ?? '')) {
         http_response_code(403);
-        die('❌ Ошибка CSRF: неверный токен');
+        die('Ошибка CSRF: неверный токен');
     }
 
     $email = htmlspecialchars($_POST['email']);
